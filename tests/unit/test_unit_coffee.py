b1ba8341598fcd4e88c04d1faf6457f7a90b3d82
Mark Mulli <markmulli001@gmail.com>
2025-04-15 11:21:01 +0300

    set_balance

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,6 +1,8 @@
 import boa
 from eth_utils import to_wei
 
+SEND_VALUE = to_wei(1, "ether")
+
 def test_price_feed_is_correct(coffee, price_feed):
     assert coffee.PRICE_FEED() == price_feed.address
 
@@ -11,3 +13,9 @@ def test_starting_values(coffee, account):
 def test_fund_fails_without_enough_eth(coffee):
     with boa.reverts("You need to spend more ETH!"):
         coffee.fund()
+        
+def test_fund_with_money(coffee, account):
+    boa.env.set_balance(account.address, SEND_VALUE)
+    coffee.fund(value = SEND_VALUE)
+    funder = coffee.funders(0)
+    assert coffee.funder_to_amount_funded(funder) == SEND_VALUE
